d92b01a1d976d8df558756bfc9e27e8e6f879844
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-07-04 06:08:19 +0300

    Add project files.

---
+++ Zoo/animal/Bird.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public class Bird : Reproduces_with_eggs
+    {
+        Reproduces_with_eggs reproduces_With_Eggs { get; set; }
+        Reproduces_with_eggs reproduces_With_Eggs1 { get; set; }
+
+        public Bird()
+        {
+
+        }
+
+    }
+}
